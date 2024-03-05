9a7e195c20f43842e3f965bdce2f1b27df9325c1
cyb3rmutant <abuhommos0@gmail.com>
2024-03-05 16:41:59 +0000

    dockerized the project and migrated to postgresql

--- surgery/settings.py
+++ surgery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-teog9+rg5n954&x+6(c_)h%6d0!miz#@_ob5*i8q$zq_&5ss6r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -75,9 +75,17 @@ WSGI_APPLICATION = "surgery.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "test",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
